2fe6ed91c63bcb1a25ecf14b33efb287cf5ef03d
kevinmarkman <kevin.w.markman@gmail.com>
2023-03-24 18:07:34 -0500

    Makes a palindrome checker using built-in JS junctions

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const makeArray = word.split('');
+  const reverseArray = makeArray.reverse();
+  const reverseWord = reverseArray.join('');
+  if(word === reverseWord){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+  //convert the sumbitted string to an array
+  //reverse the string
+  //convert array back to a string
+  //compare strings
 */
 
 /*
-  Add written explanation of your solution here
+  The isPalindrome function take a string, converts it to an array, reverses it, and converts it back to a string. Then it compares it to the orignal string. It will return true if the two are deeply equal.
 */
 
 // You can run `node index.js` to view these console logs
